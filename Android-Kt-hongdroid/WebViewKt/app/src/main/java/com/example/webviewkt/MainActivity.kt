3477edee46b0d9cb7c7d6f761e559394968d2f7b
u0min <syl8230@gmail.com>
2021-11-23 15:57:47 +0900

    Add onBackPressed function
    WebViewKt project
    webView GoBack backpages

--- Android-Kt-hongdroid/WebViewKt/app/src/main/java/com/example/webviewkt/MainActivity.kt
+++ Android-Kt-hongdroid/WebViewKt/app/src/main/java/com/example/webviewkt/MainActivity.kt
@@ -42,6 +42,17 @@ class MainActivity : AppCompatActivity() {
         binding.webView.loadUrl("https://www.naver.com") // 링크 주소를 Load 함
 
         /* 여기서 부터 추가 */
+    }
+    /* 뒤로가기 버튼 */
+    override fun onBackPressed() {
+        if(binding.webView.canGoBack()) // 웹사이트에서 뒤로 갈 페이지가 존재 한다면
+        {
+            binding.webView.goBack() // 웹사이트 뒤로가기
+        }
+        else
+        {
+            super.onBackPressed() // 본래의 백버튼 기능 수행 (안드로이드)
+        }
 
     }
 }
